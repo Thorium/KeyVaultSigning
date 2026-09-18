55bae73aa383ca8e8062632d7ea3c87cf4f9905b
Tuomas Hietanen <thorium@iki.fi>
2026-09-18 12:14:07 +0000

    fsharp-refactor code clean-up.

--- KeyVaultSignTests/Program.fs
+++ KeyVaultSignTests/Program.fs
@@ -1,8 +1,10 @@
 module Program =
     let [<EntryPoint>] main _ =
-        let testContainer = KeyVaultSignTests.TestClass()
-        testContainer.keyvault <- "isaac-hsm"
-        testContainer.certificateName <- "loantest"
+        let testContainer =
+            KeyVaultSignTests.TestClass(
+                keyvault = "isaac-hsm",
+                certificateName = "loantest"
+            )
 
         testContainer.``KeyVault sign test``()
         testContainer.``KeyVault sign and verify test``()
